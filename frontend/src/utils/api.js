2cc35ba2550fa1e73bcaef74a5766e80dd6c06cd
fa23-bcs-390-source <fa23-bcs-390@students.cuisahiwal.edu.pk>
2026-06-21 14:38:09 +0500

    Fixed new url /api

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'https://ziatradersandco-i0rt.onrender.com',
+  baseURL: process.env.REACT_APP_API_URL || 'https://ziatradersandco-i0rt.onrender.com/api',
   withCredentials: true,
 });
 
